12b891e3b6e0bb7e4585f604114d94d92e65ebbd
Thinh Nguyen <nht9d2@gmail.com>
2016-10-12 00:29:30 +0700

    Version 1.1

--- app/src/main/java/coderschool/movielist/MainActivity.java
+++ app/src/main/java/coderschool/movielist/MainActivity.java
@@ -2,12 +2,15 @@ package coderschool.movielist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView lvMovie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvMovie = findViewById(R.id.lvMovie);
+
     }
 }
